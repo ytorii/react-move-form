69eaa3cad4e37cd9a39e8f2e07f516c87756eaed
ytorii <metalwarrior_type_jp@hotmail.com>
2018-02-22 15:01:19 +0900

    Add sync validtion

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+const validate = values => {
+  const errors = {}
+  if (!values.todoText) {
+    errors.todoText = 'Required'
+  } else if (values.todoText.length > 100) {
+    errors.todoText = 'Must be 100 characters or less'
+  } else if (values.todoText.length < 5) {
+    errors.todoText = 'Must be 5 characters or more'
+  }
+
+  return errors
+}
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
 const TodoForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props;
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label>Todo</label>
         <div>
           <Field
-            name="todoText"
-            component="input"
-            type="text"
-            placeholder="Input your TODO"
+            name='todoText'
+            component={renderField}
+            type='text'
+            label='Add your TODOS!'
           />
         </div>
       </div>
       <div>
-        <button type="submit" disabled={pristine || submitting}>Submit</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
+        <button type='submit' disabled={pristine || submitting}>Submit</button>
+        <button type='button' disabled={pristine || submitting} onClick={reset}>
           Clear Values
         </button>
       </div>
@@ -26,4 +53,7 @@ const TodoForm = props => {
   );
 };
 
-export default reduxForm({ form: 'todoForm' })(TodoForm)
+export default reduxForm({
+  form: 'todoForm',
+  validate,
+})(TodoForm)
--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { connect } from 'react-redux'
 
 import TodoListComponent from '../components/TodoList'
