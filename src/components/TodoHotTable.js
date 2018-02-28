8098e48f2991145ecff75c2a63d05d246e1cb893
ytorii <metalwarrior_type_jp@hotmail.com>
2018-02-28 10:45:19 +0900

    Add HotInstance Ref

--- src/components/TodoHotTable.js
+++ src/components/TodoHotTable.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import HotTable from 'react-handsontable'
 
 const headers = [ 'ID', 'やること', '優先度', 'いつから', '期限', '終わった？' ]
@@ -18,30 +18,37 @@ const columns = [
 ]
 
 
-export default props => {
-  return (
-    <div style={{padding: '20px' }} >
-      <HotTable
-        data={props.todos}
-        colHeaders={headers}
-        columns={columns}
-        columnSorting
-        stretchH="all"
-        onAfterChange={ (c, s) => {
-          if (s !== 'loadData'){
-            console.log(this)
-            const id = props.todos[c[0][0]].id
-            console.log(id)
-          }
-        }}
-        onAfterSelection={ (r, c) => {
-          const id = props.todos[r].id
-          console.log(id)
-        }}
-        onAfterColumnSort={ (cs, t) => {
-          console.log(cs, t)
-        }}
-      />
-    </div>
+export default class TodoHotTable extends Component {
+  getCellValue = (row, column) => (
+    this.refs.hot.hotInstance.getDataAtCell(row, column)
   )
+
+  getIdValue = row => this.getCellValue(row, 0)
+
+  render(){
+    return (
+      <div style={{padding: '20px' }} >
+        <HotTable
+          root='hot'
+          ref='hot'
+          data={this.props.todos}
+          colHeaders={headers}
+          columns={columns}
+          columnSorting
+          stretchH='all'
+          onAfterChange={ (c, s) => {
+            if (s !== 'loadData'){
+              const row = c[0][0]
+              const id = this.getIdValue(row)
+              console.log(id)
+            }
+          }}
+          onAfterSelection={ (r, c) => {
+            const id = this.getIdValue(r)
+            console.log(id)
+          }}
+        />
+      </div>
+    )
+  }
 }
